86520e20873f7e04aee45fddb4061ecb4eb70339
farzi <mycodingprojects7@gmail.com>
2020-09-04 01:34:12 +0530

    initial commit 1

---
+++ src/Sdata.jsx
@@ -0,0 +1,58 @@
+const Sdata = [
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp4056398.png",
+    title: "A Netflix original series",
+    sname: "DARK",
+    link: "https://www.netflix.com/in/title/80100172",
+  },
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp1839582.jpg",
+    title: "A Netflix original series",
+    sname: "Stranger Things",
+    link: "https://www.netflix.com/in/title/80057281",
+  },
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp2084607.jpg",
+    title: "A Netflix original series",
+    sname: "BoJack Horseman",
+    link: "https://www.netflix.com/in/title/70300800",
+  },
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp3963442.jpg",
+    title: "A Netflix original series",
+    sname: "Breaking Bad",
+    link: "https://www.netflix.com/in/title/70143836",
+  },
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp2096849.jpg",
+    title: "A Netflix original series",
+    sname: "Black Mirror",
+    link: "https://www.netflix.com/in/title/70264888",
+  },
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp3099398.jpg",
+    title: "A Netflix original series",
+    sname: "Money Heist",
+    link: "https://www.netflix.com/in/title/80192098",
+  },
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp1935743.jpg",
+    title: "A Netflix original series",
+    sname: "13 Reasons Why",
+    link: "https://www.netflix.com/in/title/80117470",
+  },
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp3677227.jpg",
+    title: "A Netflix original series",
+    sname: "Arrow",
+    link: "https://www.netflix.com/in/title/70242081",
+  },
+  {
+    imgsrc: "https://wallpapercave.com/wp/wp1811395.png",
+    title: "A Netflix original series",
+    sname: "Narcos",
+    link: "https://www.netflix.com/in/title/80025172",
+  },
+];
+
+export default Sdata;
